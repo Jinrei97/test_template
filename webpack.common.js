8006b75c5ea007f52aa2a5da943528b43eada42f
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-07-20 19:08:14 +0200

    Template finito:     - eslint     - webpack-html-plugin     - style-loader / css-loader     - webpack-dev-server     - lodash     - date-fns

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
